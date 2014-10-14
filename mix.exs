56643c70cb40ca02b771bc7742460f448db969a1
Paul Wilson <paulanthonywilson@gmail.com>
2014-10-14 20:01:30 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DailyTrello.Mixfile do
 
   def project do
     [app: :daily_trello,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0.0",
      escript: escript_config,
      deps: deps]
